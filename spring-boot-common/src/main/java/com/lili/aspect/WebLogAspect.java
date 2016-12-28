667aa0a14bbf6414cbbc1ae7b202d1ce0ecc8702
lili1990 <942212309@qq.com>
2016-12-28 19:36:55 +0800

    Web层的日志切面

---
+++ spring-boot-common/src/main/java/com/lili/aspect/WebLogAspect.java
@@ -0,0 +1,59 @@
+package com.lili.aspect;
+
+import org.apache.log4j.Logger;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+
+/**
+ * Created by lili19289 on 2016/12/28.
+ */
+@Aspect
+@Component
+public class WebLogAspect {
+
+    private Logger logger = Logger.getLogger(getClass());
+
+    @Pointcut("execution(* com.lili.controller.*.*(..))")
+    public void webLog(){
+        System.out.print("----------------");
+    }
+
+    @Around(value = "webLog()")
+    public void webLog(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        long startTime = System.currentTimeMillis();
+        // 接收到请求，记录请求内容
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        StringBuilder sb = new StringBuilder();
+        // 记录下请求内容
+        sb.append("请求url>>>> " ).append( request.getRequestURL().toString()).append("\n");
+        sb.append("请求method>>> " ).append( joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName()).append("\n");
+        sb.append("请求args >>>" ).append(Arrays.toString(joinPoint.getArgs())).append("\n");
+        Object result = null;
+        try {
+            result = joinPoint.proceed();
+            sb.append("result>>>>>>>>").append(result);
+        } finally {
+            long endTime = System.currentTimeMillis();
+            sb.append( " >>>>>>>> spend time ").append(endTime - startTime).append("millis");
+            logger.error( sb.toString());
+            MDC.clear();
+        }
+        logger.info(sb.toString());
+    }
+//    @AfterReturning(returning = "returnValue", pointcut = "webLog()")
+//    public void doAfterReturning(Object returnValue) throws Throwable {
+//        // 处理完请求，返回内容
+//        logger.info("请求结果 : " + returnValue);
+//    }
+
+}
